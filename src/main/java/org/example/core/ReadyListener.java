35c4a668bf1bf16dee616cf084f84bf9a65ce8f0
ChauAry21 <aryanachauhan21@gmail.com>
2023-09-16 23:40:41 -0700

    replies and transcribes dm messages

--- src/main/java/org/example/core/ReadyListener.java
+++ src/main/java/org/example/core/ReadyListener.java
@@ -5,8 +5,11 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 // https://discord.com/api/oauth2/authorize?client_id=1151248919842537513&permissions=8&scope=bot
 
@@ -15,11 +18,12 @@ public class ReadyListener implements EventListener
     public static void main(String[] args)
             throws InterruptedException
     {
-        JDA jda = JDABuilder.createDefault("Token").addEventListeners(new ReadyListener()).build();
+        JDA jda = JDABuilder.createDefault("MTE1MTI0ODkxOTg0MjUzNzUxMw.GmXXqv.C893hxP_t3OfURcCNWJqfyiW5zS0vprmcjhETM", GatewayIntent.MESSAGE_CONTENT, GatewayIntent.DIRECT_MESSAGES).addEventListeners(new ReadyListener()).build();
         jda.getPresence().setStatus(OnlineStatus.ONLINE);
         jda.getPresence().setActivity(Activity.competing("Deranking!"));
 
         jda.addEventListener(new MyListener());
+        jda.addEventListener(new Test());
 
         jda.awaitReady();
     }
@@ -28,6 +32,14 @@ public class ReadyListener implements EventListener
     {
         if (event instanceof ReadyEvent)
             System.out.println("API is ready!");
+    }
+
+    public static class Test extends ListenerAdapter
+    {
+        public void onMessageReceived(MessageReceivedEvent event)
+        {
+            System.out.println(event.getAuthor() + event.getMessage().getContentDisplay());
+        }
     }
 
 }
