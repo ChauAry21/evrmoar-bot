a375fc07c883da51966c07248ebd0465ce9cec25
ChauAry21 <aryanachauhan21@gmail.com>
2023-09-25 03:54:29 -0700

    updated EvrmoarBot.java and MyListener.java to allow the prefix from the .env file to be used publicly

--- src/main/java/org/example/core/EvrmoarBot.java
+++ src/main/java/org/example/core/EvrmoarBot.java
@@ -3,9 +3,13 @@ package org.example.core;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.exceptions.InvalidTokenException;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.example.core.listeners.MyListener;
 
 import javax.security.auth.login.LoginException;
@@ -14,32 +18,36 @@ public class EvrmoarBot {
 
     private final ShardManager shardControl;
     private final Dotenv config;
+
+
+
+    public ShardManager getShardControl() {
+        return shardControl;
+    }
+
     public EvrmoarBot() throws LoginException {
-        config = Dotenv.configure().load();
+        config = Dotenv.configure().systemProperties().load();
 
         DefaultShardManagerBuilder build = DefaultShardManagerBuilder.createDefault(config.get("TOKEN"));
 
         build.setStatus(OnlineStatus.ONLINE);
         build.setActivity(Activity.competing("Deranking!"));
-        build.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES);
+        build.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES);
+        build.setMemberCachePolicy(MemberCachePolicy.ALL);
+        build.setChunkingFilter(ChunkingFilter.ALL);
+        build.enableCache(CacheFlag.ONLINE_STATUS);
+
         shardControl = build.build();
-
         shardControl.addEventListener(new MyListener());
-    }
-
-    public Dotenv getConfig() {
-        return config;
-    }
-
-    public ShardManager getShardControl() {
-        return shardControl;
     }
 
     public static void main(String[] args) {
         try {
             EvrmoarBot evrmoar = new EvrmoarBot();
         } catch (LoginException e) {
-            System.out.println("INVALID BOT TOKEN!");
+            System.out.println("\u001B[31m" + "INVALID BOT TOKEN!" + "\u001B[0m");
+        } catch (InvalidTokenException e) {
+            System.out.println("\u001B[31m" + "INVALID BOT TOKEN!" + "\u001B[0m");
         }
     }
 
--- src/main/java/org/example/core/listeners/MyListener.java
+++ src/main/java/org/example/core/listeners/MyListener.java
@@ -1,12 +1,21 @@
 package org.example.core.listeners;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MyListener extends ListenerAdapter {
+
+    public void onReady(ReadyEvent event) {
+        System.out.println("\u001B[32m" + "online...." + "\u001B[0m");
+        System.out.println("\u001B[36m" + "prefix: " + System.getProperty("PREFIX") + "\u001B[0m");
+    }
+
     public void onMessageReceived(MessageReceivedEvent event) {
+
         if (event.getAuthor().isBot()) {
             return;
         }
@@ -14,7 +23,8 @@ public class MyListener extends ListenerAdapter {
         Message message = event.getMessage();
         String content = message.getContentRaw();
 
-        if (content.equalsIgnoreCase("!ping")) {
+        if (content.equalsIgnoreCase( (System.getProperty("PREFIX")) + "ping")) {
+
             MessageChannel channel = event.getChannel();
             channel.sendMessage("pong!").queue();
         }
