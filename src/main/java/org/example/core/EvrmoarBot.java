ea66e0c1ac03d1d81293b2d5316735d3890f8771
ChauAry21 <aryanachauhan21@gmail.com>
2023-09-25 02:52:26 -0700

    moved MyListener.java to listeners package and added enabled gateway intents

--- src/main/java/org/example/core/EvrmoarBot.java
+++ src/main/java/org/example/core/EvrmoarBot.java
@@ -1,24 +1,15 @@
 package org.example.core;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.session.ReadyEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import org.example.core.listeners.MyListener;
 
 import javax.security.auth.login.LoginException;
 
-
-
-//config = Dotenv.configure().load();
 public class EvrmoarBot {
 
     private final ShardManager shardControl;
@@ -27,9 +18,13 @@ public class EvrmoarBot {
         config = Dotenv.configure().load();
 
         DefaultShardManagerBuilder build = DefaultShardManagerBuilder.createDefault(config.get("TOKEN"));
+
         build.setStatus(OnlineStatus.ONLINE);
         build.setActivity(Activity.competing("Deranking!"));
+        build.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES);
         shardControl = build.build();
+
+        shardControl.addEventListener(new MyListener());
     }
 
     public Dotenv getConfig() {
--- src/main/java/org/example/core/listeners/MyListener.java
+++ src/main/java/org/example/core/listeners/MyListener.java
@@ -1,22 +1,22 @@
-package org.example.core;
+package org.example.core.listeners;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class MyListener extends ListenerAdapter
-{
-    public void onMessageReceived(MessageReceivedEvent event)
-    {
-        if (event.getAuthor().isBot()) return;
+public class MyListener extends ListenerAdapter {
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
         Message message = event.getMessage();
         String content = message.getContentRaw();
-        if (content.equalsIgnoreCase("!ping"))
-        {
+
+        if (content.equalsIgnoreCase("!ping")) {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("pong!").queue();
-
         }
     }
 }
